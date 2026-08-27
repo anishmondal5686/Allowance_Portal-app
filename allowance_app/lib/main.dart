4e084c650e31a42b0f22b376c012778121a5966b
IamANISH <anish@allowance.app>
2026-08-27 13:21:32 +0530

    chore: bump version to 2.0.10

--- allowance_app/lib/main.dart
+++ allowance_app/lib/main.dart
@@ -19,7 +19,7 @@ class AllowanceApp extends StatefulWidget {
 }
 
 class _AllowanceAppState extends State<AllowanceApp> {
-  static const _appVersion = '2.0.9';
+  static const _appVersion = '2.0.10';
   final ClaimData _claimData = ClaimData();
   final DriveService _driveService = DriveService();
   final ThemeStore _themeStore = ThemeStore();
